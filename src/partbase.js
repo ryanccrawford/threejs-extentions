4ab0d9879322b6aad653f7503a26f17af5a1d128
ryanccrawford <ryanccrawford@live.com>
2020-01-02 14:01:36 -0500

    update

--- src/partbase.js
+++ src/partbase.js
@@ -2,59 +2,62 @@ import * as THREE from "three";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 
 class PartBase {
+    
     static id = -1;
-    name;
-    importFile;
-    originPart;
-    loader = null;
+    
+    name = '';
+    importFile = '';
+    partMesh;
+    loader = new FBXLoader();
     material;
-
+    height;
+    width;
+    position;
+    isImportComplete = false;
 
     constructor(options = new PartOptions()) {
         self.id++;
         this.name = options.name;
         this.material = options.material;
         this.importFile = options.importFile;
-        this.loader = new FBXLoader();
         if (this.importFile) {
-            this.loader(this.importComplete);
+            this.fileImporter(this.importComplete);
         }
 
     }
 
     importComplete = (part) => {
-
-
+        this.height = part.height;
+        this.width = part.width;
+        this.position = part.position;
+        this.partMesh = part;
+        this.isImportComplete = true;
     }
 
-    loader = (callback) => {
-        if (!this.importFile) {
+    fileImporter = (callback) => {
+        if (!this.importFile || this.isImportComplete) {
             return;
         }
-
-
         let binder = this;
-        this.loader.load(importFile, function(object) {
-            const eMap = new THREE.CubeTextureLoader()
-                .setPath("assets/3dmodels/images/cmap/")
-                .load(["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"]);
-            binder.partMaterial = new THREE.MeshLambertMaterial({
-                color: 0xfeb74c,
-                envMap: eMap,
-                reflectivity: 0.7
-            });
-
+        this.loader.load(this.importFile, function(object) {
             object.traverse(function(child) {
                 if (child.isMesh) {
                     child.castShadow = true;
                     child.receiveShadow = true;
-                    child.MeshLambertMaterial = binder.partMaterial;
+                    child.MeshLambertMaterial = binder.material;
                 }
             });
-
+            
             callback(object);
         });
     };
+
+    clone = () => {
+        if (!this.isImportComplete) {
+            return;
+        }
+        return this.partMesh.clone(true);
+    }
 }
 
 class PartOptions {
@@ -62,3 +65,5 @@ class PartOptions {
     material;
     importFile;
 }
+
+export default PartBase;
--- src/threedbuilder.js
+++ src/threedbuilder.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+import TowerSection from 'towersection.js'
 
 class thebuilder {
     scene;
