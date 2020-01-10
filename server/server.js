2a51fadafc58669809e0cab79d750f3376f76727
Ryan Crawford <ryanccrawford@live.com>
2020-01-10 06:55:58 -0500

    update

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
 
-<<<<<<< HEAD
 class Part extends Model {}
 Part.init({
     // attributes
@@ -27,21 +26,4 @@ Part.init({
 }, {
     sequelize,
     modelName: 'part'
-=======
-class User extends Model {}
-User.init({
-    // attributes
-    firstName: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    lastName: {
-        type: Sequelize.STRING
-            // allowNull defaults to true
-    }
-}, {
-    sequelize,
-    modelName: 'user'
-        // options
->>>>>>> f86f6d4... update
 });
